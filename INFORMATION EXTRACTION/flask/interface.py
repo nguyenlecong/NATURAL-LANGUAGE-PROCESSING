b9164ce4cc7e3bc7711396d6b94a7906285cca26
Le Cong Nguyen <nguyenlc_intern@arrow-tech.vn>
2021-05-28 11:36:17 +0700

    update NLP repo

---
+++ INFORMATION EXTRACTION/flask/interface.py
@@ -0,0 +1,25 @@
+import sys
+sys.path.append('D:/Tai Lieu/HUST-Study/20202/NLP/project/code')
+
+from flask import Flask, request, render_template
+from url_input import main
+from NLP import findAll
+
+app = Flask(__name__)
+ 
+@app.route('/')
+def my_form():
+    return render_template('homepage.html')
+
+@app.route('/', methods=['POST'])
+def information():
+    url = request.form['text']
+    infor = main(url)
+    typeE, cateE, ae, road, dt, ct, pr, number, fullname = findAll(infor) 
+    return render_template('homepage.html', output=infor, input=url,typeE = ', '.join(typeE),\
+                                            cateE = ', '.join(cateE),ae=', '.join(ae),road=', '.join(road),\
+                                            dt=', '.join(dt),ct =', '.join(ct), pr=', '.join(pr),\
+                                            number=', '.join(str(i) for i in number), fullname=', '.join(fullname))
+
+if __name__ == '__main__':
+    app.run()
